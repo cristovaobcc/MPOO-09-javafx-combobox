619f0f34bbd303e6831c3567a7858a97fc3d3771
cristovaobcc <cristovao.bcc@gmail.com>
2021-03-31 12:34:10 -0300

    Instancia os atributos necessários para carregar a comboBoxPessoa
    Note que o método initialize carrega os dados da comboBox.

--- src/view/ViewController.java
+++ src/view/ViewController.java
@@ -3,12 +3,46 @@
  */
 package view;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import model.entities.Pessoa;
 
 /**
  * classe controladora da View.fxml
  *
  */
-public class ViewController {
+public class ViewController implements Initializable{
+	
+	@FXML
+	private ComboBox<Pessoa> comboxBoxPessoa;
+	
+	private ObservableList<Pessoa> obsList;
+	
+	
+	
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		
+		List<Pessoa> list = new ArrayList<>();
+		list.add(new Pessoa(1, "Maria", "maria@gmail.com"));
+		list.add(new Pessoa(2, "João", "joao@gmail.com"));
+		list.add(new Pessoa(3, "Bob", "bob@gmail.com"));
+		
+		// Instancia o objt ObservableList<Pessoa> desta classe.
+		obsList = FXCollections.observableArrayList(list); 
+		// Carrega os dados da obsList no comboBox.
+		comboxBoxPessoa.setItems(obsList);
+		
+	}
 	
 	
 	
