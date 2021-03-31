d6243bd6e80f70b1a9d3db2e99e5ea9c427d89d5
cristovaobcc <cristovao.bcc@gmail.com>
2021-03-31 12:08:58 -0300

    Refatora o projeto para seguir conforme a aula do curso.

---
+++ src/view/ViewController.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package view;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+
+/**
+ * classe controladora da View.fxml
+ *
+ */
+public class ViewController {
+	
+	
+	
+}
